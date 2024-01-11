18afc1dc2a3164c40c2c145a82f3728aa3385f19
Brian Ngugi <143685881+Brianthe2nd@users.noreply.github.com>
2024-01-11 21:52:59 +0300

    Update light_2.js

--- light_2.js
+++ light_2.js
@@ -55,7 +55,7 @@ const fetchLighthouseReports = async (urls, apiKey, Queue) => {
 
     progressBar.start(totalRequests, 0);
 
-    const queue = new Queue({ concurrency: 30 });
+    const queue = new Queue({ concurrency: 20 });
 
     const reportPromises = validUrls.map(url => queue.add(() => fetchLighthouseReport(url, apiKey, progressBar, Queue)));
 
